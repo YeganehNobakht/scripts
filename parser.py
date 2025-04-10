e4d5c6270b0524f373515f126a0a4f6c865660fb
YeganehNobakht <m.yeganeh.n1994@gmail.com>
2025-04-10 22:19:06 +0330

    feat: toggling file type

--- parser.py
+++ parser.py
@@ -1,19 +1,20 @@
+import os
 import json 
 import yaml 
 
-def json_parser(file_name):
+def parse_json(file_name):
     with open(file_name, "r") as file:
         file_content = json.load(file)
     return file_content;
 
-def yaml_parser(file_name):
+def parse_yml(file_name):
     with open(file_name, "r") as file:
         file_content = yaml.safe_load(file)
     return file_content;
 
 
 def print_yml_file(file_name):
-    yml_content = yaml_parser(file_name)
+    yml_content = parse_yml(file_name)
     print
     for key , value in yml_content.items():
         print(f"key is: {key}")
@@ -25,7 +26,7 @@ def write_json_to_file(content, dest_file):
         json.dump(content, file, indent=4)
 
 def convert_yml_to_json(yml_file_name, json_file_name):
-    yml_content = yaml_parser(yml_file_name)
+    yml_content = parse_yml(yml_file_name)
     write_json_to_file(yml_content, json_file_name)
 
 def write_yml_to_file(content, dest_file):
@@ -33,14 +34,23 @@ def write_yml_to_file(content, dest_file):
         yaml.dump(content, file, indent=4)
 
 def convert_json_to_yml(json_file_name, dest_yml_file):
-    yml_content = json_parser(json_file_name)
+    yml_content = parse_json(json_file_name)
     write_yml_to_file(yml_content, dest_yml_file)
+
+def toggle_file_type(file_name, converted_file):
+    name, ext = os.path.splitext(file_name)
+    if ext == ".yml" or ext == ".yaml":
+        convert_yml_to_json(file_name, converted_file)
+    elif ext == ".json":
+        convert_json_to_yml(file_name, converted_file)
+
 
 def main():
     # print(json_parser("info.json"))
     # print_yml_file("info.yml")
     # convert_yml_to_json("info.yml", "yml_to_json.json")
-    convert_json_to_yml("info.json", "json_to_yml.yml")
+    # convert_json_to_yml("info.json", "json_to_yml.yml")
+    toggle_file_type("info.json", "toggle.yml")
 
 
 
