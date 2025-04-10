36d5ae9bffb77f1d19403237efcf067c0858c1af
YeganehNobakht <m.yeganeh.n1994@gmail.com>
2025-04-10 19:28:29 +0330

    feat: convert yml to json

--- parser.py
+++ parser.py
@@ -1,14 +1,14 @@
-from json import load, dumps
-from yaml import dump, safe_load
+import json 
+import yaml 
 
 def json_parser(file_name):
     with open(file_name, "r") as file:
-        file_content = load(file)
+        file_content = json.load(file)
     return file_content;
 
 def yaml_parser(file_name):
     with open(file_name, "r") as file:
-        file_content = safe_load(file)
+        file_content = yaml.safe_load(file)
     return file_content;
 
 
@@ -19,9 +19,20 @@ def print_yml_file(file_name):
         print(f"key is: {key}")
         print(f"value is: {value}")
 
+
+def write_json_to_file(content, dest_file):
+    with open(dest_file, "w") as file:
+        json.dump(content, file, indent=4)
+
+def convert_yml_to_json(yml_file_name, json_file_name):
+    yml_content = yaml_parser(yml_file_name)
+    write_json_to_file(yml_content, json_file_name)
+
 def main():
     # print(json_parser("info.json"))
-    print_yml_file("info.yml")
+    # print_yml_file("info.yml")
+    convert_yml_to_json("info.yml", "yml_to_json.json")
+
 
 
 
